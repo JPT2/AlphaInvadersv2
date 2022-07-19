041f5488fab91d417dbb2ba5428c5f244ea296f0
ptaggs <ptaggs@gmail.com>
2022-07-18 21:19:48 -0700

    Trying new approach. Test file for environment.

---
+++ jamSrc/envTest.py
@@ -0,0 +1,18 @@
+# Test file for playing with the environment
+
+import gym
+
+WILL_RENDER = True
+
+# Program definition
+if WILL_RENDER:
+    env = gym.make('ALE/SpaceInvaders-v5', full_action_space=False, render_mode='human')
+else:
+    env = gym.make('ALE/SpaceInvaders-v5', full_action_space=False, render_mode='human')
+
+observation = env.reset()
+done = False
+
+while not done:
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
